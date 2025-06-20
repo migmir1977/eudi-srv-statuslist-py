afc93b375406da0621631e3d9ddbd5fc25e133ff
Miguel Miranda <90401421+migmir1977@users.noreply.github.com>
2025-06-20 12:14:02 +0100

    Update status_list_endpoints.py

--- app/status_list_endpoints.py
+++ app/status_list_endpoints.py
@@ -42,12 +42,13 @@ def take_index():
     if api_key != current_app.config['API_key']:
         return jsonify({"message": "Unauthorized access"}), 401
 
-    external_doctype = request.form.get("doctype", "").strip()    
-    ALLOWED_DOCTYPES = {"json"}
-    if external_doctype not in ALLOWED_DOCTYPES:
-        abort(400, "Invalid doctype")    
-    else
-        doctype="json"
+    doctype = request.form["doctype"]
+    #external_doctype = request.form.get("doctype", "").strip()    
+    #ALLOWED_DOCTYPES = {"json"}
+    #if external_doctype not in ALLOWED_DOCTYPES:
+    #    abort(400, "Invalid doctype")    
+    #else
+    #    doctype="json"
         
     country = request.form["country"]
     expiry_date = request.form["expiry_date"]
